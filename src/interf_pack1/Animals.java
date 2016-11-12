dac0cd1b177df04bd3ff06f2488d1605fc01a83a
makassigithub <my123gate@gmail.com>
2016-11-11 22:08:14 -0500

    Update Animals.java

--- src/interf_pack1/Animals.java
+++ src/interf_pack1/Animals.java
@@ -3,6 +3,8 @@ package interf_pack1;
 public interface Animals {
 	
 	// This is just to show that an interface can define a variable which must be static and final
+	// the variables even if there are declared STATIC FINAL, there are implicitly static final.
+	// Also remeber an interface can not maintain a state information like classes.
 	public static final String GENERAL_TYPE = " AMINAL";
 	 
 	public void showType();
